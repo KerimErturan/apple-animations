d00863e86e36632864417c7c7781a93f49b9861f
wskirichenko <ws.kirichenko@gmail.com>
2020-10-22 10:37:34 +0300

    Added animation scalability

--- src/js/main.js
+++ src/js/main.js
@@ -9,45 +9,51 @@
   var clearControllers = () => {
     controlMain.destroy();
   };
+  var getSize = () => {
+    windowHight = $(window).height();
+    windowWidth = $(window).width();
+    iPhonHeight = $('#iphone').innerHeight();
+    sectFirst = $('.first-sect').innerHeight();
+    console.log('sectFirst=',sectFirst);
+  }
 
   var animateMain = () => {
     controlMain = new ScrollMagic.Controller();
 
-    var tweenBack = TweenMax.staggerFromTo(".animate-sect__iphon", 1,
-        {  top: 900  },
-        {  top: -800 },
-        0.15);
-    var backScena = new ScrollMagic.Scene({
-        triggerElement: "#start_trigger", duration: 1400+1400, offset: 10
-    }).setTween(tweenBack)
-    .addIndicators({name: "01"});
-
-    var tweenBackText = TweenMax.staggerFromTo(".animate-sect__text", 1,
-        {  top: 900  },
-        {  top: 0 },
-        0.15);
-    var backTextScena = new ScrollMagic.Scene({
-        triggerElement: "#start_trigger", duration: 1400, offset: 1000
-    }).setTween(tweenBackText)
-    .addIndicators({name: "02"});
-
     var tweenMainText = TweenMax.staggerFromTo(".animate-sect__hed ", 1,
-        {  top: 550  },
-        {  top: 50 },
+        {  top: sectFirst  },
+        {  top: 100 },
         0.15);
     var textMainScena = new ScrollMagic.Scene({
-        triggerElement: "#start_trigger", duration: 900, offset: -100
+        triggerElement: "#start_trigger",
+        duration: sectFirst,
+        offset: -100,
+        ease: 'none'
     }).setTween(tweenMainText)
-    .addIndicators({name: "text"});
+    // .addIndicators({name: "text"});
 
     var tweenColorText = TweenMax.staggerFromTo(".animate-sect__hed__h2", 1,
         {  color: 'white'  },
         {  color: 'black' },
         0.15);
     var textColorScena = new ScrollMagic.Scene({
-        triggerElement: "#start_trigger", duration: 300, offset: 600
+        triggerElement: "#start_trigger", duration: 300, offset: sectFirst/2
     }).setTween(tweenColorText)
-    .addIndicators({name: "textColor"});
+    // .addIndicators({name: "textColor"});
+
+    // --- iPhon --------------
+
+    var tweenBack = TweenMax.staggerFromTo(".animate-sect__iphon", 1,
+        { top: windowHight },
+        { top: -(iPhonHeight+20) },
+        0.15);
+    var backScena = new ScrollMagic.Scene({
+        triggerElement: "#start_trigger",
+        duration      : (windowHight + 2 * iPhonHeight),
+        offset        : -100,
+        ease: 'none'
+    }).setTween(tweenBack)
+    // .addIndicators({name: "iPhone"});
 
     // --- Block animation --------------
 
@@ -56,36 +62,36 @@
         { width: 300, height: 120, marginLeft: 25 },
         0.15);
     var block01_Scena = new ScrollMagic.Scene({
-        triggerElement: "#start_trigger", duration: 200, offset: 100+500
+        triggerElement: "#start_trigger", duration: 200, offset: 200
     }).setTween(tweenBlock01)
-    .addIndicators({name: "block_01_Scena"});
+    // .addIndicators({name: "block_01_Scena"});
 
     var tweenBlock02 = TweenMax.staggerFromTo(".block-02", 1,
         { width: 500, height: 200, marginLeft: -75, marginTop: 375 },
         { width: 300, height: 120, marginLeft: 25, marginTop: 325  },
         0.15);
     var block02_Scena = new ScrollMagic.Scene({
-        triggerElement: "#start_trigger", duration: 200, offset: 100+500+100
+        triggerElement: "#start_trigger", duration: 200, offset: 100+200
     }).setTween(tweenBlock02)
-    .addIndicators({name: "block_02_Scena"});
+    // .addIndicators({name: "block_02_Scena"});
 
     var tweenBlock03 = TweenMax.staggerFromTo(".block-03", 1,
         { width: 500, height: 200, marginLeft: -75, marginTop: 550 },
         { width: 300, height: 120, marginLeft: 25, marginTop: 450  },
         0.15);
     var block03_Scena = new ScrollMagic.Scene({
-        triggerElement: "#start_trigger", duration: 200, offset: 100+500+100+50
+        triggerElement: "#start_trigger", duration: 200, offset: 100+200+50
     }).setTween(tweenBlock03)
-    .addIndicators({name: "block_03_Scena"});
+    // .addIndicators({name: "block_03_Scena"});
 
     var tweenBlock04 = TweenMax.staggerFromTo(".block-04", 1,
         { width: 500, height: 200, marginLeft: -75, marginTop: 700 },
         { width: 300, height: 120, marginLeft: 25, marginTop: 575  },
         0.15);
     var block04_Scena = new ScrollMagic.Scene({
-        triggerElement: "#start_trigger", duration: 200, offset: 100+500+100+100
+        triggerElement: "#start_trigger", duration: 200, offset: 100+200+100
     }).setTween(tweenBlock04)
-    .addIndicators({name: "block_04_Scena"});
+    // .addIndicators({name: "block_04_Scena"});
 
     // --- Text animation --------------
 
@@ -94,7 +100,7 @@
         { fontSize: 14 },
         0.15);
     var text_Scena01 = new ScrollMagic.Scene({
-        triggerElement: "#start_trigger", duration: 200, offset: 100+500
+        triggerElement: "#start_trigger", duration: 200, offset: 200
     }).setTween(textAnim01)
 
     var textAnim02 = TweenMax.staggerFromTo(".text-anim-02", 1,
@@ -102,7 +108,7 @@
         { fontSize: 14 },
         0.15);
     var text_Scena02 = new ScrollMagic.Scene({
-        triggerElement: "#start_trigger", duration: 200, offset: 100+500+100
+        triggerElement: "#start_trigger", duration: 200, offset: 100+200
     }).setTween(textAnim02)
 
     var textAnim03 = TweenMax.staggerFromTo(".text-anim-03", 1,
@@ -110,7 +116,7 @@
         { fontSize: 14 },
         0.15);
     var text_Scena03 = new ScrollMagic.Scene({
-        triggerElement: "#start_trigger", duration: 200, offset: 100+500+100+50
+        triggerElement: "#start_trigger", duration: 200, offset: 100+200+50
     }).setTween(textAnim03)
 
     var textAnim04 = TweenMax.staggerFromTo(".text-anim-04", 1,
@@ -118,18 +124,32 @@
         { fontSize: 14 },
         0.15);
     var text_Scena04 = new ScrollMagic.Scene({
-        triggerElement: "#start_trigger", duration: 200, offset: 100+500+100+100
+        triggerElement: "#start_trigger", duration: 200, offset: 100+200+100
     }).setTween(textAnim04)
+
+    // ---- End text -------------
+
+    var tweenBackText = TweenMax.staggerFromTo(".animate-sect__text", 1,
+        {  top: windowHight  },
+        {  top: 0 },
+        0.15);
+    var backTextScena = new ScrollMagic.Scene({
+        triggerElement: "#start_trigger", duration: windowHight*2, offset: windowHight
+    }).setTween(tweenBackText)
+    // .addIndicators({name: "02"});
 
 
     controlMain.addScene([
-      backScena, textMainScena, textColorScena, backTextScena,
+      backScena, textMainScena, textColorScena,
       block01_Scena, block02_Scena, block03_Scena, block04_Scena,
-      text_Scena01, text_Scena02, text_Scena03, text_Scena04
+      text_Scena01, text_Scena02, text_Scena03, text_Scena04,
+      backTextScena,
     ]);
   }
 
   // ---- Beginning Parallax animation for first loading -------
+  getSize();
+
   if (isMobail.matches) {
     console.log('mobile');
     animateMain();
@@ -137,4 +157,14 @@
     console.log('desctop');
     animateMain();
   }
+
+  // ---- Parallax animation for resize -------
+  $(window).resize(() => {
+    getSize();
+    if (isMobail.matches) {
+      animateMain();
+    } else {
+      animateMain();
+    }
+  });
 // })
